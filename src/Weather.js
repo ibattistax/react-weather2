533d0bb9f30a32260df6ce2185c02114af4c4202
ibattistax <isabel.battista@gmail.com>
2024-07-07 15:37:20 +0100

    Deleted reference to WeatherForecast before it is used.

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
 import moment from "moment";
 // import moment from "moment-timezone";
