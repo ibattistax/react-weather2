6e96de5ba1a948239de0d7dfccb37c06a05e50e9
ibattistax <isabel.battista@gmail.com>
2024-07-13 20:47:31 +0100

    Fixed bug preventing netlify build

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -33,6 +33,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
