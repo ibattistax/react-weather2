6de422d17a3ba92ee9d4e9eec8cb152a661cf017
ibattistax <isabel.battista@gmail.com>
2024-07-22 21:39:08 +0100

    Minor css changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Weather from "./Weather";
 import PageFooter from "./PageFooter";
+import Logo1 from "./logo1.jpg";
 
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="logo-icon-container">
+        <img
+          src={Logo1}
+          alt="GirlWeather Logo"
+          width={400}
+          className="d-block mx-auto img-fluid w-50"
+        />
+      </div>
       <div className="weather-app">
         <Weather defaultCity="LONDON" />
       </div>
