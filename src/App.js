9c5e32f8c69c37860e25cd1f70e17afdedc71f96
ibattistax <isabel.battista@gmail.com>
2024-09-09 07:36:09 +0100

    Deleted logo

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="logo-icon-container">
-        <img
-          src={Logo1}
-          alt="GirlWeather Logo"
-          width={400}
-          className="d-block mx-auto img-fluid w-50"
-        />
-      </div>
       <div className="weather-app">
         <Weather defaultCity="LONDON" />
       </div>
